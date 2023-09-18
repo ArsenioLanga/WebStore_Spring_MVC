d6c14ac9185427e8c0cf0646c14c4aaf312de6e7
ARSENIO <ARSENIO@ARSENIO-LANGA-SSI.ge.mz>
2023-09-18 13:49:42 +0100

    Initial Project WebStore

---
+++ WebAppStore/src/main/java/webappstore/WebAppStoreApplication.java
@@ -0,0 +1,13 @@
+package webappstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppStoreApplication.class, args);
+	}
+
+}
---
+++ WebAppStore/src/test/java/webappstore/WebAppStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package webappstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
