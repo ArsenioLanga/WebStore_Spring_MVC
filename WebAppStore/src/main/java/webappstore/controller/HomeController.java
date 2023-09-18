29790d1738d5e64916321173bfadf155c23d2e98
ARSENIO <ARSENIO@ARSENIO-LANGA-SSI.ge.mz>
2023-09-18 15:07:45 +0100

    Reset Project

---
+++ WebAppStore/src/main/java/webappstore/controller/HomeController.java
@@ -0,0 +1,10 @@
+package webappstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+}
